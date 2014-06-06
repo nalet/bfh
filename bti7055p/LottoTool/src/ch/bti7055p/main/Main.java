ea8c6d483ae14f54a83727564b5cb19328b00792
nibill <bucheggert@outlook.com>
2014-06-06 18:22:07 +0200

    various changes

--- bti7055p/LottoTool/src/ch/bti7055p/main/Main.java
+++ bti7055p/LottoTool/src/ch/bti7055p/main/Main.java
@@ -1,20 +1,30 @@
 package ch.bti7055p.main;
 	
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Locale locale = new Locale("de", "CH");
+	
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../views/MainView.fxml"));
-			Scene scene = new Scene(root,400,400);
+		try {			
+			AnchorPane root = (AnchorPane)FXMLLoader.load(
+					Main.class.getResource("../views/MainView.fxml"),
+					ResourceBundle.getBundle("lang", locale));
+			
+			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("LottoTool");
+			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -26,33 +36,4 @@ public class Main extends Application {
 		launch(args);
 	}
 }
-
-//public class Main extends Application {
-//	
-//	private static Locale locale = new Locale("de", "CH");
-//	
-//	@Override
-//	public void start(Stage primaryStage) {
-//		try {
-//			//AnchorPane root = (AnchorPane)FXMLLoader.load(Main.class.getResource("Main.fxml"));
-//			
-//			AnchorPane root = (AnchorPane)FXMLLoader.load(
-//					Main.class.getResource("Main.fxml"),
-//					ResourceBundle.getBundle("lang", locale));
-//			
-//			Scene scene = new Scene(root,600,600);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.setTitle("Lotto");
-//			primaryStage.setResizable(false);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
-//}
 
