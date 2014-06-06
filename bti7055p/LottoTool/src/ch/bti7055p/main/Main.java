f14ab108255bd74c3ef946974fe10fc637bef83c
nibill <bucheggert@outlook.com>
2014-06-06 18:06:00 +0200

    Main.java

--- bti7055p/LottoTool/src/ch/bti7055p/main/Main.java
+++ bti7055p/LottoTool/src/ch/bti7055p/main/Main.java
@@ -2,8 +2,9 @@ package ch.bti7055p.main;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.PopupWindow.AnchorLocation;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -11,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../views/MainView.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../views/MainView.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("LottoTool");
@@ -26,3 +27,44 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
+package application;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+//public class Main extends Application {
+//	
+//	private static Locale locale = new Locale("de", "CH");
+//	
+//	@Override
+//	public void start(Stage primaryStage) {
+//		try {
+//			//AnchorPane root = (AnchorPane)FXMLLoader.load(Main.class.getResource("Main.fxml"));
+//			
+//			AnchorPane root = (AnchorPane)FXMLLoader.load(
+//					Main.class.getResource("Main.fxml"),
+//					ResourceBundle.getBundle("lang", locale));
+//			
+//			Scene scene = new Scene(root,600,600);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.setTitle("Lotto");
+//			primaryStage.setResizable(false);
+//			primaryStage.show();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		launch(args);
+//	}
+//}
+
